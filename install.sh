11a12f8f0c0e7bdaf0353b7561c0e19fda6214f5
Kevin Luo <kevin.luo@linecorp.com>
2017-07-19 14:04:12 +0800

    Update install scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CONFIG_HOME=$HOME/.mac-config
+CONFIG_HOME=$HOME/.mac-zsh-config
 
 warn() {
     echo "$1" >&2
@@ -13,34 +13,31 @@ die() {
 [ -e $CONFIG_HOME ] && die "$CONFIG_HOME already exists."
 
 git clone git@github.com:xfalcons/bash-git-prompt.git .bash-git-prompt
-git clone git@github.com:xfalcons/mac-config.git "$CONFIG_HOME"
+git clone git@github.com:xfalcons/mac-zsh-config.git "$CONFIG_HOME"
 
 cd ~
 [ ! -d "bin" ] && mkdir -p bin
-[ ! -d "mac-config.bak" ] && mkdir -p mac-config.bak
+[ ! -d "mac-zsh-config.bak" ] && mkdir -p mac-zsh-config.bak
 
-target_files=".bash_profile .bashrc .gemrc .tmux.conf .gitconfig .gitexcludes .gitignore_global bin/git_differ_wrapper"
+target_files=".bash_profile .bashrc .zshrc .tmux.conf .gitconfig .gitexcludes .gitignore_global bin/git_differ_wrapper"
 for f in $target_files
 do
     if [ -e $f ]
     then
         mv $f mac-config.bak/
-        echo "Move $f to mac-config.bak/"
+        echo "Move $f to mac-zsh-config.bak/"
     fi
 done
 
-ln -s .mac-config/bash_profile .bash_profile
-ln -s .mac-config/bashrc .bashrc
-ln -s .mac-config/gemrc .gemrc
-ln -s .mac-config/tmux.conf .tmux.conf
-ln -s .mac-config/gitconfig .gitconfig
-ln -s .mac-config/gitexcludes .gitexcludes
-ln -s .mac-config/gitignore_global .gitignore_global
-ln -s .mac-config/bin/git_diff_wrapper bin/git_diff_wrapper
+ln -s .mac-zsh-config/bash_profile .bash_profile
+ln -s .mac-zsh-config/bashrc .bashrc
+ln -s .mac-zsh-config/zshrc .zshrc
+ln -s .mac-zsh-config/tmux.conf .tmux.conf
+ln -s .mac-zsh-config/gitconfig .gitconfig
+ln -s .mac-zsh-config/gitexcludes .gitexcludes
+ln -s .mac-zsh-config/gitignore_global .gitignore_global
+ln -s .mac-zsh-config/bin/git_diff_wrapper bin/git_diff_wrapper
 
 # wget -O - https://raw.github.com/xfalcons/vimrc/master/auto-install.sh | sh
-
-# On Ubuntu, there is no /usr/bin/ctags in default
-sudo apt-get install exuberant-ctags
 
 echo "$HOME configuration file has been installed."
